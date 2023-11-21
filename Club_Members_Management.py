b8db4344f8ba5aff987535c207a27243e5d0a65f
chera mihiretu <cheramihiretucm@gmail.com>
2023-11-21 01:31:58 -0800

    Creating the Main Frame

--- Club_Members_Management.py
+++ Club_Members_Management.py
@@ -1,1 +1,11 @@
-
+from customtkinter import *
+# The Main frame
+class MainFrame(CTk):
+    def __init__(self, title):
+        CTk.__init__(self)
+        self.title(title)
+        self.geometry("400x400")
+#Starting the main loop
+if __name__ == "__main__":
+    app = MainFrame("CMM")
+    app.mainloop()
